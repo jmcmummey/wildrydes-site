ce013710e6b48c06918cddad4ee8a51e92c8456a
Jesse McMullen-Crummey <jesse@Jesses-MacBook-Pro.local>
2021-11-23 11:19:07 -0500

    try cognito again

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
-\window._config = {
+window._config = {
     cognito: {
-        userPoolId: 'ca-central-1_YFNrFRgal', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '10j5h9e3es26kh43vp1irt2i16', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ca-central-1_1toGIctxS', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '69qvseln7if2jcsj8enbd6834j', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
