ee4e5867893337a5c36cd2b2a3bb7f3891b23df8
Jesse McMullen-Crummey <jesse@Jesses-MacBook-Pro.local>
2021-11-23 17:21:28 -0500

    Added API

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://n4edht4rib.execute-api.ca-central-1.amazonaws.com/Prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
