059c2cc15a963dc5fc71a897842441ece9399704
Jesse McMullen-Crummey <jesse@Jesses-MacBook-Pro.local>
2021-11-23 11:09:50 -0500

    new cognito app

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 \window._config = {
     cognito: {
         userPoolId: 'ca-central-1_YFNrFRgal', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'lvp0vivi290ojoanchgfuabhi', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '10j5h9e3es26kh43vp1irt2i16', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ca-central-1' // e.g. us-east-2
     },
     api: {
